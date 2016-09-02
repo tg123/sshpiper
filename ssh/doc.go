2b8d5e725ee4e641d6c640764e54225129d18be8
Boshi Lian <farmer1992@gmail.com>
2016-09-02 12:05:35 -0700

    import latest golang.org/x/crypto/ssh

--- ssh/doc.go
+++ ssh/doc.go
@@ -3,9 +3,12 @@
 // license that can be found in the LICENSE file.
 
 /*
+Package ssh implements an SSH client and server.
+
 SSHPiper provides additional APIs to ssh that allows you to build a piped ssh connection.
 Also, package ssh in sshpiper is compatible with golang.org/x/crypto/ssh.
 All func and datatype left unchanged. You can use it like golang.org/x/crypto/ssh.
+
 
 SSH is a transport security protocol, an authentication protocol and a
 family of application protocols. The most typical application level
@@ -14,7 +17,7 @@ the multiplexed nature of SSH is exposed to users that wish to support
 others.
 
 References:
-  [PROTOCOL.certkeys]: http://www.openbsd.org/cgi-bin/cvsweb/src/usr.bin/ssh/PROTOCOL.certkeys
+  [PROTOCOL.certkeys]: http://cvsweb.openbsd.org/cgi-bin/cvsweb/src/usr.bin/ssh/PROTOCOL.certkeys?rev=HEAD
   [SSH-PARAMETERS]:    http://www.iana.org/assignments/ssh-parameters/ssh-parameters.xml#ssh-parameters-1
 */
 package ssh
