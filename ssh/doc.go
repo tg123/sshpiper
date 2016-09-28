bf39d5cd9f2ad3e08dfaa1c1b9f1c3f65ebe51d8
Boshi Lian <farmer1992@gmail.com>
2016-09-28 05:08:55 -0700

    fix import path checking

--- ssh/doc.go
+++ ssh/doc.go
@@ -5,6 +5,11 @@
 /*
 Package ssh implements an SSH client and server.
 
+SSHPiper provides additional APIs to ssh that allows you to build a piped ssh connection.
+Also, package ssh in sshpiper is compatible with golang.org/x/crypto/ssh.
+All func and datatype left unchanged. You can use it like golang.org/x/crypto/ssh.
+
+
 SSH is a transport security protocol, an authentication protocol and a
 family of application protocols. The most typical application level
 protocol is a remote shell and this is specifically implemented.  However,
@@ -15,4 +20,4 @@ References:
   [PROTOCOL.certkeys]: http://cvsweb.openbsd.org/cgi-bin/cvsweb/src/usr.bin/ssh/PROTOCOL.certkeys?rev=HEAD
   [SSH-PARAMETERS]:    http://www.iana.org/assignments/ssh-parameters/ssh-parameters.xml#ssh-parameters-1
 */
-package ssh // import "golang.org/x/crypto/ssh"
+package ssh
