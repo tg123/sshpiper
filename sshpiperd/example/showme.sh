c44625ac70be843075ecdc331953a21e35638a14
Boshi Lian <farmer1992@gmail.com>
2019-01-07 10:55:33 +0000

    showme.sh is now using piper tools

--- sshpiperd/example/showme.sh
+++ sshpiperd/example/showme.sh
@@ -1,32 +1,55 @@
 #!/bin/bash
+set -e
 
-if [ -z $GOPATH ]; then
-    echo "set go path first"
-    exit 1
-fi
-
-echo "Using go path $GOPATH"
-
+GOPATH=`go env GOPATH`
 
 if [ ! -f $GOPATH/bin/sshpiperd ] || [ $GOPATH/bin/sshpiperd -ot $GOPATH/src/github.com/tg123/sshpiper ];then
-    go install github.com/tg123/sshpiper/sshpiperd
+    go get github.com/tg123/sshpiper/sshpiperd
 fi
 
 SSHPIPERD_BIN="$GOPATH/bin/sshpiperd"
 BASEDIR="$GOPATH/src/github.com/tg123/sshpiper/sshpiperd/example"
 
 if [ ! -f $BASEDIR/sshpiperd_key ];then
-    ssh-keygen -N '' -f $BASEDIR/sshpiperd_key
+    $SSHPIPERD_BIN genkey > $BASEDIR/sshpiperd_key
 fi
 
-for u in `find $BASEDIR/workingdir/ -name sshpiper_upstream`; do
-    chmod 400 $u
-    upstream=`cat $u`
 
-    username=`dirname $u`
-    username=`basename $username`
 
-    echo "ssh 127.0.0.1 -p 2222 -l $username # connect to $upstream"
+$SSHPIPERD_BIN pipe --upstream-workingdir=$BASEDIR/workingdir add -n github -u github.com 2>/dev/null
+$SSHPIPERD_BIN pipe --upstream-workingdir=$BASEDIR/workingdir add -n gitlab -u gitlab.com 2>/dev/null
+$SSHPIPERD_BIN pipe --upstream-workingdir=$BASEDIR/workingdir add -n bitbucket -u bitbucket.org 2>/dev/null
+
+IFS="
+"
+
+echo "#### CURRENT PIPES"
+echo "# "
+echo "# test using ssh 127.0.0.1 -p 2222 -l username"
+echo 
+
+for p in `$SSHPIPERD_BIN pipe --upstream-workingdir=$BASEDIR/workingdir list`; do
+    user=`echo $p | cut -f 1 -d ' '`
+    echo "$p # ssh 127.0.0.1 -p 2222 -l $user"
 done
 
-$SSHPIPERD_BIN -i $BASEDIR/sshpiperd_key -w $BASEDIR/workingdir
+
+
+echo 
+echo "#### "
+
+echo 
+
+echo "Starting piper"
+
+#for u in `find $BASEDIR/workingdir/ -name sshpiper_upstream`; do
+#    chmod 400 $u
+#    upstream=`cat $u`
+#
+#    username=`dirname $u`
+#    username=`basename $username`
+#
+#    echo "ssh 127.0.0.1 -p 2222 -l $username # connect to $upstream"
+#done
+
+$SSHPIPERD_BIN daemon -i $BASEDIR/sshpiperd_key --upstream-workingdir=$BASEDIR/workingdir
