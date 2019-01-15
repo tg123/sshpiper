efab28ea53781272968644ddb9e18239d4fe49cf
Boshi Lian <farmer1992@gmail.com>
2019-01-15 15:35:10 +0000

    remove unused comment

--- sshpiperd/e2e/piper.sh
+++ sshpiperd/e2e/piper.sh
@@ -1,8 +1,6 @@
 #!/bin/sh
 
-echo 1
 sleep 10 # TODO remove ulgy workaround 
-echo 2
 
 /sshpiperd pipe add -n host1 -u host1 --upstream-username root
 /sshpiperd pipe add -n host2 -u host2 --upstream-username root
