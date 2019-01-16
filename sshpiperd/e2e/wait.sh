d4c96ea951d5a6e931b994fd07dfeac6c08af162
Boshi Lian <farmer1992@gmail.com>
2019-01-16 12:03:13 +0000

    add mssql support

--- sshpiperd/e2e/wait.sh
+++ sshpiperd/e2e/wait.sh
@@ -1,7 +1,6 @@
 #!/bin/sh
 
 for i in `seq 300`; do
-    nc -w 1 -z "$1" "$2" >/dev/null 2>&1
     nc -w 1 -z "$1" "$2"
     if [ $? -eq 0 ]; then
         break
---
+++ sshpiperd/upstream/database/mssql.go
@@ -0,0 +1,58 @@
+package database
+
+import (
+	"fmt"
+	"net/url"
+
+	"github.com/jinzhu/gorm"
+	_ "github.com/jinzhu/gorm/dialects/mssql"
+
+	upstreamprovider "github.com/tg123/sshpiper/sshpiperd/upstream"
+)
+
+type mssqlplugin struct {
+	plugin
+
+	Config struct {
+		Host     string `long:"upstream-mssql-host" default:"127.0.0.1" description:"sql server host for driver" env:"SSHPIPERD_UPSTREAM_MSSQL_HOST" ini-name:"upstream-mssql-host"`
+		User     string `long:"upstream-mssql-user" default:"sa" description:"sql server user for driver" env:"SSHPIPERD_UPSTREAM_MSSQL_USER" ini-name:"upstream-mssql-user"`
+		Password string `long:"upstream-mssql-password" default:"" description:"sql server password for driver" env:"SSHPIPERD_UPSTREAM_MSSQL_PASSWORD" ini-name:"upstream-mssql-password"`
+		Port     uint   `long:"upstream-mssql-port" default:"1433" description:"sql server port for driver" env:"SSHPIPERD_UPSTREAM_MSSQL_PORT" ini-name:"upstream-mssql-port"`
+		Dbname   string `long:"upstream-mssql-dbname" default:"sshpiper" description:"sql server dbname for driver" env:"SSHPIPERD_UPSTREAM_MSSQL_DBNAME" ini-name:"upstream-mssql-dbname"`
+		Instance string `long:"upstream-mssql-instance" description:"sql server db instance" env:"SSHPIPERD_UPSTREAM_MSSQL_INSTANCE" ini-name:"upstream-mssql-instance"`
+	}
+}
+
+func (p *mssqlplugin) create() (*gorm.DB, error) {
+	query := url.Values{}
+	query.Add("database", p.Config.Dbname)
+
+	u := &url.URL{
+		Scheme:   "sqlserver",
+		User:     url.UserPassword(p.Config.User, p.Config.Password),
+		Host:     fmt.Sprintf("%s:%d", p.Config.Host, p.Config.Port),
+		Path:     p.Config.Instance,
+		RawQuery: query.Encode(),
+	}
+
+	db, err := gorm.Open("mssql", u.String())
+	if err != nil {
+		return nil, err
+	}
+
+	return db, nil
+}
+
+func (mssqlplugin) GetName() string {
+	return "mssql"
+}
+
+func (p *mssqlplugin) GetOpts() interface{} {
+	return &p.Config
+}
+
+func init() {
+	p := &mssqlplugin{}
+	p.createdb = p
+	upstreamprovider.Register("mssql", p)
+}
