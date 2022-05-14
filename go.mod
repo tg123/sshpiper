9285824798116ebf619a8d1b2fce15907fe1f40c
Boshi Lian <farmer1992@gmail.com>
2022-05-14 13:14:10 +0000

    update adal to az identity

--- go.mod
+++ go.mod
@@ -8,19 +8,19 @@ replace (
 )
 
 require (
-	github.com/Azure/azure-sdk-for-go v58.0.0+incompatible
-	github.com/Azure/go-autorest/autorest v0.11.21
-	github.com/Azure/go-autorest/autorest/adal v0.9.16
+	github.com/Azure/azure-sdk-for-go/sdk/azidentity v1.0.0
 	github.com/dcu/go-authy v1.0.1
 	github.com/go-sql-driver/mysql v1.6.0
 	github.com/google/uuid v1.3.0
 	github.com/jessevdk/go-flags v1.5.0
 	github.com/jinzhu/gorm v1.9.16
+	github.com/microsoft/kiota-authentication-azure-go v0.2.1
+	github.com/microsoftgraph/msgraph-sdk-go v0.23.0
 	github.com/pockost/sshpipe-k8s-lib v0.0.3
 	github.com/sirupsen/logrus v1.8.1
 	github.com/tg123/remotesigner v0.0.0-20210928104451-7c20285909d1
 	github.com/tg123/sshkey v0.0.0-20201202190454-3bb356f89f1f
-	golang.org/x/crypto v0.0.0-20210921155107-089bfa567519
+	golang.org/x/crypto v0.0.0-20220511200225-c6db032c6c88
 	google.golang.org/grpc v1.45.0
 	google.golang.org/protobuf v1.28.0
 	gopkg.in/yaml.v3 v3.0.0-20210107192922-496545a6307b
@@ -29,19 +29,17 @@ require (
 )
 
 require (
-	github.com/Azure/go-autorest v14.2.0+incompatible // indirect
-	github.com/Azure/go-autorest/autorest/date v0.3.0 // indirect
-	github.com/Azure/go-autorest/autorest/to v0.4.0 // indirect
-	github.com/Azure/go-autorest/autorest/validation v0.3.1 // indirect
-	github.com/Azure/go-autorest/logger v0.2.1 // indirect
-	github.com/Azure/go-autorest/tracing v0.6.0 // indirect
+	github.com/Azure/azure-sdk-for-go/sdk/azcore v1.0.0 // indirect
+	github.com/Azure/azure-sdk-for-go/sdk/internal v1.0.0 // indirect
+	github.com/AzureAD/microsoft-authentication-library-for-go v0.4.0 // indirect
+	github.com/cjlapao/common-go v0.0.19 // indirect
 	github.com/davecgh/go-spew v1.1.1 // indirect
 	github.com/denisenkom/go-mssqldb v0.11.0 // indirect
 	github.com/go-logr/logr v1.1.0 // indirect
 	github.com/gogo/protobuf v1.3.2 // indirect
 	github.com/gojektech/heimdall v5.0.2+incompatible // indirect
 	github.com/gojektech/valkyrie v0.0.0-20190210220504-8f62c1e7ba45 // indirect
-	github.com/golang-jwt/jwt/v4 v4.0.0 // indirect
+	github.com/golang-jwt/jwt v3.2.1+incompatible // indirect
 	github.com/golang-sql/civil v0.0.0-20190719163853-cb61b32ac6fe // indirect
 	github.com/golang/protobuf v1.5.2 // indirect
 	github.com/google/go-cmp v0.5.6 // indirect
@@ -49,14 +47,22 @@ require (
 	github.com/googleapis/gnostic v0.5.5 // indirect
 	github.com/jinzhu/inflection v1.0.0 // indirect
 	github.com/json-iterator/go v1.1.12 // indirect
+	github.com/kylelemons/godebug v1.1.0 // indirect
 	github.com/lib/pq v1.1.1 // indirect
 	github.com/mattn/go-sqlite3 v2.0.3+incompatible // indirect
+	github.com/microsoft/kiota-abstractions-go v0.5.0 // indirect
+	github.com/microsoft/kiota-http-go v0.3.0 // indirect
+	github.com/microsoft/kiota-serialization-json-go v0.3.1 // indirect
+	github.com/microsoft/kiota-serialization-text-go v0.2.0 // indirect
+	github.com/microsoftgraph/msgraph-sdk-go-core v0.24.0 // indirect
 	github.com/modern-go/concurrent v0.0.0-20180306012644-bacd9c7ef1dd // indirect
 	github.com/modern-go/reflect2 v1.0.2 // indirect
+	github.com/pkg/browser v0.0.0-20210115035449-ce105d075bb4 // indirect
 	github.com/pkg/errors v0.9.1 // indirect
-	golang.org/x/net v0.0.0-20211112202133-69e39bad7dc2 // indirect
+	github.com/yosida95/uritemplate/v3 v3.0.2 // indirect
+	golang.org/x/net v0.0.0-20220425223048-2871e0cb64e4 // indirect
 	golang.org/x/oauth2 v0.0.0-20210819190943-2bc19b11175f // indirect
-	golang.org/x/sys v0.0.0-20211003122950-b1ebd4e1001c // indirect
+	golang.org/x/sys v0.0.0-20211216021012-1d35b9e2eb4e // indirect
 	golang.org/x/term v0.0.0-20210927222741-03fcf44c2211 // indirect
 	golang.org/x/text v0.3.7 // indirect
 	golang.org/x/time v0.0.0-20210723032227-1f47c861a9ac // indirect
--- sshpiperd/challenger/azdevicecode/handler.go
+++ sshpiperd/challenger/azdevicecode/handler.go
@@ -2,52 +2,37 @@ package azdevicecode
 
 import (
 	"context"
-	"net/http"
 
 	log "github.com/sirupsen/logrus"
 
 	"golang.org/x/crypto/ssh"
 
-	"github.com/Azure/azure-sdk-for-go/services/graphrbac/1.6/graphrbac"
-	"github.com/Azure/go-autorest/autorest"
-	"github.com/Azure/go-autorest/autorest/adal"
-	"github.com/Azure/go-autorest/autorest/azure"
+	"github.com/Azure/azure-sdk-for-go/sdk/azcore/policy"
+	azidentity "github.com/Azure/azure-sdk-for-go/sdk/azidentity"
+	a "github.com/microsoft/kiota-authentication-azure-go"
+	msgraphsdk "github.com/microsoftgraph/msgraph-sdk-go"
+	"github.com/microsoftgraph/msgraph-sdk-go/models"
 )
 
 type authClient struct {
 	Config struct {
 		TenantID string `long:"challenger-azdevicecode-tenantid" description:"Azure AD tenant id" env:"SSHPIPERD_CHALLENGER_AZDEVICECODE_TENANTID" ini-name:"challenger-azdevicecode-tenantid"`
 		ClientID string `long:"challenger-azdevicecode-clientid" description:"Azure AD client id" env:"SSHPIPERD_CHALLENGER_AZDEVICECODE_CLIENTID" ini-name:"challenger-azdevicecode-clientid"`
-		// Env         string `long:"challenger-azdevicecode-env" default:"AzurePublicCloud" description:"Azure AD Cloud to request" env:"SSHPIPERD_CHALLENGER_AZDEVICECODE_ENV" ini-name:"challenger-azdevicecode-env" choice:"AzureChinaCloud" choice:"AzureGermanCloud" choice:"AzurePublicCloud" choice:"AzureUSGovernmentCloud"`
-		Env         string `long:"challenger-azdevicecode-env" default:"AzurePublicCloud" description:"Azure AD Cloud to request" env:"SSHPIPERD_CHALLENGER_AZDEVICECODE_ENV" ini-name:"challenger-azdevicecode-env"`
-		Resource    string `long:"challenger-azdevicecode-resource" default:"https://graph.windows.net/" description:"Resource URI to access, default is Graph API" env:"SSHPIPERD_CHALLENGER_AZDEVICECODE_RESOURCE" ini-name:"challenger-azdevicecode-resource"`
+		// Env         string `long:"challenger-azdevicecode-env" default:"AzurePublicCloud" description:"Azure AD Cloud to request" env:"SSHPIPERD_CHALLENGER_AZDEVICECODE_ENV" ini-name:"challenger-azdevicecode-env"`
+		Scope       string `long:"challenger-azdevicecode-scope" default:"User.Read" description:"Permission scope when querying user info" env:"SSHPIPERD_CHALLENGER_AZDEVICECODE_SCOPE" ini-name:"challenger-azdevicecode-scope"`
 		NoReadGraph bool   `long:"challenger-azdevicecode-noreadgraph" description:"Disable query user info from user graph" env:"SSHPIPERD_CHALLENGER_AZDEVICECODE_NOREADGRAPH" ini-name:"challenger-azdevicecode-noreadgraph"`
 	}
 
-	logger      *log.Logger
-	oauthConfig adal.OAuthConfig
+	logger *log.Logger
 }
 
 func (c *authClient) Init(logger *log.Logger) error {
 	c.logger = logger
-
-	env, err := azure.EnvironmentFromName(c.Config.Env)
-	if err != nil {
-		return err
-	}
-
-	oauthConfig, err := adal.NewOAuthConfig(env.ActiveDirectoryEndpoint, c.Config.TenantID)
-	if err != nil {
-		return err
-	}
-
-	c.oauthConfig = *oauthConfig
-
 	return nil
 }
 
 type aadUser struct {
-	user graphrbac.User
+	models.Userable
 }
 
 func (*aadUser) ChallengerName() string {
@@ -55,48 +40,49 @@ func (*aadUser) ChallengerName() string {
 }
 
 func (a *aadUser) Meta() interface{} {
-	return a.user
+	return a.Userable
 }
 
 func (a *aadUser) ChallengedUsername() string {
-	return *a.user.UserPrincipalName
+	return *a.GetId()
 }
 
+// see https://github.com/microsoftgraph/msgraph-sdk-go
 func (c *authClient) challenge(conn ssh.ConnMetadata, client ssh.KeyboardInteractiveChallenge) (ssh.AdditionalChallengeContext, error) {
-	oauthClient := &http.Client{}
-	deviceCode, err := adal.InitiateDeviceAuth(oauthClient, c.oauthConfig, c.Config.ClientID, c.Config.Resource)
+
+	cred, err := azidentity.NewDeviceCodeCredential(&azidentity.DeviceCodeCredentialOptions{
+		TenantID: c.Config.TenantID,
+		ClientID: c.Config.ClientID,
+		UserPrompt: func(ctx context.Context, message azidentity.DeviceCodeMessage) error {
+			_, err := client(conn.User(), message.Message, nil, nil)
+			return err
+		},
+	})
 
 	if err != nil {
 		return nil, err
 	}
 
-	_, err = client(conn.User(), *deviceCode.Message, nil, nil)
-	if err != nil {
-		return nil, err
-	}
-
-	token, err := adal.WaitForUserCompletion(oauthClient, deviceCode)
-	if err != nil {
-		return nil, err
-	}
-
-	// skip read user info from graph api
 	if c.Config.NoReadGraph {
-		return nil, nil
+		_, err = cred.GetToken(context.Background(), policy.TokenRequestOptions{
+			Scopes: []string{c.Config.Scope},
+		})
+
+		return nil, err
 	}
 
-	spt, err := adal.NewServicePrincipalTokenFromManualToken(c.oauthConfig, c.Config.ClientID, c.Config.Resource, *token)
+	auth, err := a.NewAzureIdentityAuthenticationProviderWithScopes(cred, []string{c.Config.Scope})
 	if err != nil {
 		return nil, err
 	}
 
-	signedInUserClient := graphrbac.NewSignedInUserClient(c.Config.TenantID)
-	signedInUserClient.Authorizer = autorest.NewBearerAuthorizer(spt)
-
-	result, err := signedInUserClient.Get(context.TODO())
+	adapter, err := msgraphsdk.NewGraphRequestAdapter(auth)
 	if err != nil {
 		return nil, err
 	}
 
-	return &aadUser{user: result}, nil
+	gsclient := msgraphsdk.NewGraphServiceClient(adapter)
+	result, err := gsclient.Me().Get()
+
+	return &aadUser{result}, err
 }
