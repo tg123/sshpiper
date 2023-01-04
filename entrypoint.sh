7aa657d8eb63756a2fab8dff5b274c2bf44f213d
Vlastimil Holer <vh@fortrabbit.com>
2023-01-04 15:53:36 +0100

    fix entrypoint keygen key type to Ed25519
    Related to #110 #111

--- entrypoint.sh
+++ entrypoint.sh
@@ -3,7 +3,7 @@ set -eo pipefail
 
 if [ -z "$SSHPIPERD_SERVER_KEY" ]; then
     if [ ! -f /etc/ssh/ssh_host_ed25519_key ];then
-        ssh-keygen -t rsa -N '' -f /etc/ssh/ssh_host_ed25519_key
+        ssh-keygen -t ed25519 -N '' -f /etc/ssh/ssh_host_ed25519_key
     fi
 fi
 
