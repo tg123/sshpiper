5b9091c42a16624d42094ef5734d7ff8013cf914
Boshi Lian <farmer1992@gmail.com>
2023-03-12 05:15:22 -0700

    update dep (#146)
    * update dep
    
    * update crypto
    
    * revert docker lib version

--- plugin/kubernetes/generated/clientset/versioned/clientset.go
+++ plugin/kubernetes/generated/clientset/versioned/clientset.go
@@ -17,8 +17,7 @@ type Interface interface {
 	SshpiperV1beta1() sshpiperv1beta1.SshpiperV1beta1Interface
 }
 
-// Clientset contains the clients for groups. Each group has exactly one
-// version included in a Clientset.
+// Clientset contains the clients for groups.
 type Clientset struct {
 	*discovery.DiscoveryClient
 	sshpiperV1beta1 *sshpiperv1beta1.SshpiperV1beta1Client
