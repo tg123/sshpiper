3e511325dfc99e29ddf66ff93136654a1e89f390
mattpker <mattpker@gmail.com>
2023-08-25 15:31:34 -0700

    Add further errror checking & e2e tests for new optional ca cert

--- e2e/e2eentry.sh
+++ e2e/e2eentry.sh
@@ -3,6 +3,9 @@
 # use entrypoint.sh to generate the ssh_host_ed25519_key
 PLUGIN="dummy_badname/" bash /sshpiperd/entrypoint.sh 2>/dev/null
 
+# Use the ca cert to sign the ssh_host_ed25519_key
+ssh-keygen -s cahost/ca -I sshpiper_test -n client_123 /etc/ssh/ssh_host_ed25519_key.pub
+
 if [ "${SSHPIPERD_DEBUG}" == "1" ]; then
     echo "enter debug on hold mode"
     echo "run [docker exec -ti e2e_testrunner_1 bash] to run to attach"
