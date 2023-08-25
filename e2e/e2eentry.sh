ca4c0d4ea1bf2ab48164e91a2fc0c3b4a2374578
mattpker <mattpker@gmail.com>
2023-08-25 16:51:45 -0700

    Create all the certs needed to make the ca public cert

--- e2e/e2eentry.sh
+++ e2e/e2eentry.sh
@@ -3,8 +3,10 @@
 # use entrypoint.sh to generate the ssh_host_ed25519_key
 PLUGIN="dummy_badname/" bash /sshpiperd/entrypoint.sh 2>/dev/null
 
-# Use the ca cert to sign the ssh_host_ed25519_key
-ssh-keygen -s cahost/ca -I sshpiper_test -n client_123 /etc/ssh/ssh_host_ed25519_key.pub
+# Create an ssh key pair and then sign the public key with the ca cert
+chmod 600 cahost/ca
+ssh-keygen -t ssh-ed25519 -f /etc/ssh/ssh_user -N ""
+ssh-keygen -s cahost/ca -I ssh_user -n client_123 /etc/ssh/ssh_user.pub
 
 if [ "${SSHPIPERD_DEBUG}" == "1" ]; then
     echo "enter debug on hold mode"
--- plugin/testcaplugin/main.go
+++ plugin/testcaplugin/main.go
@@ -37,13 +37,13 @@ func main() {
 					}
 
 					// Load the private key
-					signer, err := os.ReadFile("/etc/ssh/ssh_host_ed25519_key")
+					signer, err := os.ReadFile("/etc/ssh/ssh_user")
 					if err != nil {
 						return nil, fmt.Errorf("unable to read private key: %v", err)
 					}
 
 					// Load the public ca certificate
-					capublickey, err := os.ReadFile("/etc/ssh/ssh_host_ed25519_key-cert.pub")
+					capublickey, err := os.ReadFile("/etc/ssh/ssh_user-cert.pub")
 					if err != nil {
 						return nil, fmt.Errorf("unable to read ca cert: %v", err)
 					}
