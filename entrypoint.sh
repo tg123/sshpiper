d21bed7edf2a24e0831c2d66459b0224d41894d0
Vlastimil Holer <vlastimil.holer@gmail.com>
2024-02-29 12:36:56 +0100

    Support multiple plugins in entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -4,4 +4,15 @@ set -eo pipefail
 PLUGIN=${PLUGIN:-workingdir}
 export SSHPIPERD_SERVER_KEY_GENERATE_MODE=${SSHPIPERD_SERVER_KEY_GENERATE_MODE:-notexist}
 
-/sshpiperd/sshpiperd "${@:-/sshpiperd/plugins/$PLUGIN}"
+if [ $# -eq 0 ]; then
+    # $PLUGIN can contain a comma/space separated list of plugins
+    # to load (e.g., PLUGIN="kubernetes,failtoban")
+    PLUGINS=''
+    for P in $(echo "$PLUGIN" | tr ',;`$' ' '); do
+        PLUGINS="${PLUGINS}/sshpiperd/plugins/${P} -- "
+    done
+
+    exec /sshpiperd/sshpiperd ${PLUGINS}
+else
+    exec /sshpiperd/sshpiperd "${@}"
+fi
