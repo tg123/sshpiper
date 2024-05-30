6fe67e3f5c4ea39ec2f4fb1d13ba11bbb19cf378
Boshi Lian <farmer1992@gmail.com>
2024-05-30 00:18:35 -0700

    revert partial succ support (#395)
    * chore: Update Dockerfile to use golang 1.22-bookworm and install additional dependencies
    
    * revert crypto
    
    * testcase to cover re-mapping to fail

--- e2e/kubernetes_test.go
+++ e2e/kubernetes_test.go
@@ -145,4 +145,58 @@ func TestKubernetes(t *testing.T) {
 		})
 
 	}
+
+	{
+		// fallback to password
+		t.Run("fallback to password", func(t *testing.T) {
+			randtext := uuid.New().String()
+			targetfie := uuid.New().String()
+
+			keyfiledir, err := os.MkdirTemp("", "")
+			if err != nil {
+				t.Errorf("failed to create temp key file: %v", err)
+			}
+
+			keyfile := path.Join(keyfiledir, "key")
+
+			if err := runCmdAndWait(
+				"ssh-keygen",
+				"-N",
+				"",
+				"-f",
+				keyfile,
+			); err != nil {
+				t.Errorf("failed to generate key: %v", err)
+			}
+
+			c, stdin, stdout, err := runCmd(
+				"ssh",
+				"-v",
+				"-o",
+				"StrictHostKeyChecking=no",
+				"-o",
+				"UserKnownHostsFile=/dev/null",
+				"-p",
+				piperport,
+				"-l",
+				"pass",
+				"-i",
+				keyfile,
+				piperhost,
+				fmt.Sprintf(`sh -c "echo -n %v > /shared/%v"`, randtext, targetfie),
+			)
+
+			if err != nil {
+				t.Errorf("failed to ssh to piper-fixed, %v", err)
+			}
+
+			defer killCmd(c)
+
+			enterPassword(stdin, stdout, "pass")
+
+			time.Sleep(time.Second) // wait for file flush
+
+			checkSharedFileContent(t, targetfie, randtext)
+		})
+	}
 }
