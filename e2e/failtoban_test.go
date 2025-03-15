9ff3550786455733b570620ee3485832ed48a4f8
diedpigs <louistw@uab.edu>
2025-03-15 06:23:09 -0500

    Feat: fail2ban whitelist (#546)
    * feat: support whitelist in failtoban plugin
    
    * test: add e2e test for failtoban ignore ip
    
    * refactor: use netipx for easy contains check

--- e2e/failtoban_test.go
+++ e2e/failtoban_test.go
@@ -240,3 +240,235 @@ func TestFailtobanPipeCreateFail(t *testing.T) {
 		}
 	}
 }
+
+func TestFailtobanIgnoreIP(t *testing.T) {
+	piperaddr, piperport := nextAvailablePiperAddress()
+
+	piper, _, _, err := runCmd("/sshpiperd/sshpiperd",
+		"-p",
+		piperport,
+		"/sshpiperd/plugins/fixed",
+		"--target",
+		"host-password:2222",
+		"--",
+		"/sshpiperd/plugins/failtoban",
+		"--max-failures",
+		"3",
+		"--ignore-ip",
+		"127.0.0.1",
+	)
+
+	if err != nil {
+		t.Errorf("failed to run sshpiperd: %v", err)
+	}
+
+	defer killCmd(piper)
+
+	waitForEndpointReady(piperaddr)
+
+	{
+		randtext := uuid.New().String()
+		targetfie := uuid.New().String()
+
+		c, stdin, stdout, err := runCmd(
+			"ssh",
+			"-v",
+			"-o",
+			"StrictHostKeyChecking=no",
+			"-o",
+			"UserKnownHostsFile=/dev/null",
+			"-p",
+			piperport,
+			"-l",
+			"user",
+			"127.0.0.1",
+			fmt.Sprintf(`sh -c "echo -n %v > /shared/%v"`, randtext, targetfie),
+		)
+
+		if err != nil {
+			t.Errorf("failed to ssh to piper-workingdir, %v", err)
+		}
+
+		defer killCmd(c)
+
+		enterPassword(stdin, stdout, "pass")
+
+		time.Sleep(time.Second) // wait for file flush
+
+		checkSharedFileContent(t, targetfie, randtext)
+	}
+
+	// run 3 times with wrong password
+	{
+		c, stdin, stdout, err := runCmd(
+			"ssh",
+			"-v",
+			"-o",
+			"StrictHostKeyChecking=no",
+			"-o",
+			"UserKnownHostsFile=/dev/null",
+			"-p",
+			piperport,
+			"-l",
+			"user",
+			"127.0.0.1",
+		)
+		if err != nil {
+			t.Errorf("failed to ssh to piper-fixed, %v", err)
+		}
+
+		defer killCmd(c)
+
+		enterPassword(stdin, stdout, "wrongpass1")
+		enterPassword(stdin, stdout, "wrongpass2")
+		enterPassword(stdin, stdout, "wrongpass3")
+	}
+
+	{
+		randtext := uuid.New().String()
+		targetfie := uuid.New().String()
+
+		c, stdin, stdout, err := runCmd(
+			"ssh",
+			"-v",
+			"-o",
+			"StrictHostKeyChecking=no",
+			"-o",
+			"UserKnownHostsFile=/dev/null",
+			"-p",
+			piperport,
+			"-l",
+			"user",
+			"127.0.0.1",
+			fmt.Sprintf(`sh -c "echo -n %v > /shared/%v"`, randtext, targetfie),
+		)
+
+		if err != nil {
+			t.Errorf("failed to ssh to piper-workingdir, %v", err)
+		}
+
+		defer killCmd(c)
+
+		enterPassword(stdin, stdout, "pass")
+
+		time.Sleep(time.Second) // wait for file flush
+
+		checkSharedFileContent(t, targetfie, randtext)
+	}
+}
+
+func TestFailtobanIgnoreCIDR(t *testing.T) {
+	piperaddr, piperport := nextAvailablePiperAddress()
+
+	piper, _, _, err := runCmd("/sshpiperd/sshpiperd",
+		"-p",
+		piperport,
+		"/sshpiperd/plugins/fixed",
+		"--target",
+		"host-password:2222",
+		"--",
+		"/sshpiperd/plugins/failtoban",
+		"--max-failures",
+		"3",
+		"--ignore-ip",
+		"127.0.0.1/8",
+	)
+
+	if err != nil {
+		t.Errorf("failed to run sshpiperd: %v", err)
+	}
+
+	defer killCmd(piper)
+
+	waitForEndpointReady(piperaddr)
+
+	{
+		randtext := uuid.New().String()
+		targetfie := uuid.New().String()
+
+		c, stdin, stdout, err := runCmd(
+			"ssh",
+			"-v",
+			"-o",
+			"StrictHostKeyChecking=no",
+			"-o",
+			"UserKnownHostsFile=/dev/null",
+			"-p",
+			piperport,
+			"-l",
+			"user",
+			"127.0.0.1",
+			fmt.Sprintf(`sh -c "echo -n %v > /shared/%v"`, randtext, targetfie),
+		)
+
+		if err != nil {
+			t.Errorf("failed to ssh to piper-workingdir, %v", err)
+		}
+
+		defer killCmd(c)
+
+		enterPassword(stdin, stdout, "pass")
+
+		time.Sleep(time.Second) // wait for file flush
+
+		checkSharedFileContent(t, targetfie, randtext)
+	}
+
+	// run 3 times with wrong password
+	{
+		c, stdin, stdout, err := runCmd(
+			"ssh",
+			"-v",
+			"-o",
+			"StrictHostKeyChecking=no",
+			"-o",
+			"UserKnownHostsFile=/dev/null",
+			"-p",
+			piperport,
+			"-l",
+			"user",
+			"127.0.0.1",
+		)
+		if err != nil {
+			t.Errorf("failed to ssh to piper-fixed, %v", err)
+		}
+
+		defer killCmd(c)
+
+		enterPassword(stdin, stdout, "wrongpass1")
+		enterPassword(stdin, stdout, "wrongpass2")
+		enterPassword(stdin, stdout, "wrongpass3")
+	}
+
+	{
+		randtext := uuid.New().String()
+		targetfie := uuid.New().String()
+
+		c, stdin, stdout, err := runCmd(
+			"ssh",
+			"-v",
+			"-o",
+			"StrictHostKeyChecking=no",
+			"-o",
+			"UserKnownHostsFile=/dev/null",
+			"-p",
+			piperport,
+			"-l",
+			"user",
+			"127.0.0.1",
+			fmt.Sprintf(`sh -c "echo -n %v > /shared/%v"`, randtext, targetfie),
+		)
+
+		if err != nil {
+			t.Errorf("failed to ssh to piper-workingdir, %v", err)
+		}
+
+		defer killCmd(c)
+
+		enterPassword(stdin, stdout, "pass")
+
+		time.Sleep(time.Second) // wait for file flush
+
+		checkSharedFileContent(t, targetfie, randtext)
+	}
+}
--- plugin/failtoban/main.go
+++ plugin/failtoban/main.go
@@ -4,9 +4,12 @@ package main
 
 import (
 	"fmt"
+	"go4.org/netipx"
 	"net"
+	"net/netip"
 	"os"
 	"os/signal"
+	"strings"
 	"syscall"
 	"time"
 
@@ -40,14 +43,21 @@ func main() {
 				EnvVars: []string{"SSHPIPERD_FAILTOBAN_LOG_ONLY"},
 				Value:   false,
 			},
+			&cli.StringSliceFlag{
+				Name:    "ignore-ip",
+				Usage:   "ignore ip, will not ban host matches from these ip addresses",
+				EnvVars: []string{"SSHPIPERD_FAILTOBAN_IGNORE_IP"},
+				Value:   cli.NewStringSlice(),
+			},
 		},
 		CreateConfig: func(c *cli.Context) (*libplugin.SshPiperPluginConfig, error) {
 
 			maxFailures := c.Int("max-failures")
 			banDuration := c.Duration("ban-duration")
 			logOnly := c.Bool("log-only")
-
+			ignoreIP := c.StringSlice("ignore-ip")
 			cache := gocache.New(banDuration, banDuration/2*3)
+			whitelist := buildIPSet(ignoreIP)
 
 			// register signal handler
 			go func() {
@@ -74,6 +84,12 @@ func main() {
 					}
 
 					ip, _, _ := net.SplitHostPort(conn.RemoteAddr())
+					ip0, _ := netip.ParseAddr(ip)
+
+					if whitelist.Contains(ip0) {
+						log.Debugf("failtoban: %v in whitelist, ignored.", ip0)
+						return nil
+					}
 
 					failed, found := cache.Get(ip)
 					if !found {
@@ -89,15 +105,57 @@ func main() {
 				},
 				UpstreamAuthFailureCallback: func(conn libplugin.ConnMetadata, method string, err error, allowmethods []string) {
 					ip, _, _ := net.SplitHostPort(conn.RemoteAddr())
+					ip0, _ := netip.ParseAddr(ip)
+
+					if whitelist.Contains(ip0) {
+						log.Debugf("failtoban: %v in whitelist, ignored.", ip0)
+						return
+					}
+
 					failed, _ := cache.IncrementInt(ip, 1)
 					log.Warnf("failtoban: %v auth failed. current status: fail %v times, max allowed %v", ip, failed, maxFailures)
 				},
 				PipeCreateErrorCallback: func(remoteAddr string, err error) {
 					ip, _, _ := net.SplitHostPort(remoteAddr)
+					ip0, _ := netip.ParseAddr(ip)
+
+					if whitelist.Contains(ip0) {
+						log.Debugf("failtoban: %v in whitelist, ignored.", ip0)
+						return
+					}
+
 					failed, _ := cache.IncrementInt(ip, 1)
 					log.Warnf("failtoban: %v pipe create failed, reason %v. current status: fail %v times, max allowed %v", ip, err, failed, maxFailures)
 				},
 			}, nil
 		},
 	})
+}
+
+func buildIPSet(cidrs []string) *netipx.IPSet {
+
+	var ipsetBuilder netipx.IPSetBuilder
+
+	for _, cidr := range cidrs {
+		if strings.Contains(cidr, "/") {
+			prefix, err := netip.ParsePrefix(cidr)
+			if err != nil {
+				log.Debugf("failtoban: error while parsing ignore IP: \n%v", err)
+				continue
+			}
+			ipsetBuilder.AddPrefix(prefix)
+		} else {
+			ip, err := netip.ParseAddr(cidr)
+			if err != nil {
+				log.Debugf("failtoban: error while parsing ignore IP: \n%v", err)
+				continue
+			}
+			ipsetBuilder.Add(ip)
+		}
+	}
+	ipset, err := ipsetBuilder.IPSet()
+	if err != nil {
+		log.Debugf("failtoban: error while getting IPSet: \n%v", err)
+	}
+	return ipset
 }
