ae91559131d72738a9ee950aa48e2c249da68dd6
Boshi Lian <farmer1992@gmail.com>
2025-06-21 23:35:10 -0700

    refactor(libplugin): update GetOrGenerateUri to return error and add IPv6 test

--- libplugin/plugin_ext.go
+++ libplugin/plugin_ext.go
@@ -1,18 +1,23 @@
 package libplugin
 
-import "fmt"
+import (
+	"fmt"
+	"net"
+	"strconv"
+)
 
 // GetOrGenerateUri returns the existing Uri if set, otherwise constructs it from Host and Port.
-func (x *Upstream) GetOrGenerateUri() string {
+func (x *Upstream) GetOrGenerateUri() (string, error) {
 	uri := x.GetUri()
 	if uri != "" {
-		return uri
+		return uri, nil
 	}
 
-	port := x.Port
+	port := x.GetPort()
 	if port <= 0 {
 		port = 22
 	}
+	addr := net.JoinHostPort(x.GetHost(), strconv.Itoa(int(port)))
 
-	return fmt.Sprintf("tcp://%v:%v", x.Host, port)
+	return fmt.Sprintf("tcp://%v", addr), nil
 }
---
+++ libplugin/plugin_ext_test.go
@@ -0,0 +1,18 @@
+package libplugin
+
+import "testing"
+
+func TestGetOrGenerateUri_GeneratesUriWithIPv6(t *testing.T) {
+	up := &Upstream{
+		Host: "2001:db8::1",
+		Port: 2222,
+	}
+
+	got, err := up.GetOrGenerateUri()
+	if err != nil {
+		t.Fatalf("unexpected error: %v", err)
+	}
+	if got != "tcp://[2001:db8::1]:2222" {
+		t.Errorf("expected generated uri, got %q", got)
+	}
+}
