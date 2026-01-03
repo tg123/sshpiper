cc678268e5e1b48752cb444467d6d881336d0b1e
Boshi Lian <farmer1992@gmail.com>
2026-01-03 09:21:19 -0800

    refactor: improve e2eentry script for benchmark test execution

--- e2e/e2eentry.sh
+++ e2e/e2eentry.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-set -x
+set -xe
 
 groupadd -f testgroup && \
 useradd -m -G testgroup testgroupuser
@@ -8,6 +8,10 @@ if [ "${SSHPIPERD_DEBUG}" == "1" ]; then
     echo "enter debug on hold mode"
     echo "run [docker exec -ti e2e_testrunner_1 bash] to run to attach"
     sleep infinity; 
-else 
-    go test -v -bench .; 
+else
+    echo "running tests" 
+    go test -v .
+    
+    echo "running benchmarks"
+    go test -v -bench=. -run=^$ -count=100 .;
 fi
