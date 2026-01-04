e5d31ac50e5697d776d4c2e9570524389ab3e1d9
Boshi Lian <farmer1992@gmail.com>
2026-01-04 05:16:24 -0800

    refactor: add support for skipping e2e tests and adjust benchmark duration

--- e2e/e2eentry.sh
+++ e2e/e2eentry.sh
@@ -9,9 +9,16 @@ if [ "${SSHPIPERD_DEBUG}" == "1" ]; then
     echo "run [docker exec -ti e2e_testrunner_1 bash] to run to attach"
     sleep infinity; 
 else
-    echo "running tests" 
-    go test -v .
+
+    if [ "${SSHPIPERD_SKIP_E2E}" == "1" ]; then
+        echo "skipping e2e tests as requested"
+    else
+        echo "running tests" 
+        go test -v .
+    fi
     
-    echo "running benchmarks"
-    go test -v -bench=. -run=^$ -benchtime=1m .;
+    if [ "${SSHPIPERD_BENCHMARKS}" == "1" ]; then
+        echo "running benchmarks"
+        go test -v -bench=. -run=^$ -benchtime=20s .;
+    fi
 fi
